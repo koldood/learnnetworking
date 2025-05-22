0b97ec8992ad9494dddc4682ebd7e3ed8c0bd263
Mikah <mikah.mulube@gmail.com>
2025-05-22 09:55:09 +0100

    added threading to the server

--- tcp_server.py
+++ tcp_server.py
@@ -1,7 +1,22 @@
 import socket
+import threading
+
+def handle_client(connection, client_address):
+    try:
+        print('Connection from', client_address)
+        while True:
+            data = connection.recv(16)
+            if data:
+                print('Received {!r}'.format(data))
+                connection.sendall(data)
+            else:
+                print('No more data from', client_address)
+                break
+    finally:
+        connection.close()
 
 
-def start_tcp_server():
+def start_threaded_tcp_server():
     """
     Start a TCP server that listens for incoming requests and echoes back what
     it receives.
@@ -9,32 +24,18 @@ def start_tcp_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_address = ('localhost', 65432)
-    print('Starting up on {} port {}'.format(*server_address))
     server_socket.bind(server_address)
 
-    server_socket.listen(1)
+    server_socket.listen(5)
+    print('Waiting for a connection')
 
     while True:
-        print('Waiting for a connection')
         connection, client_address = server_socket.accept()
-        try:
-            print('Connection from', client_address)
-
-            while True:
-                data = connection.recv(16)
-                print('Received {!r}'.format(data))
-                if data:
-                    print('Sending data back to the client')
-                    connection.sendall(data)
-                else:
-                    print('No more data from', client_address)
-                    break
-        finally:
-            connection.close()
-
+        client_thread = threading.Thread(target=handle_client, args=(connection, client_address))
+        client_thread.start()
 
 
 # the lines below are our entrypoint - you can run this script by running
 # `python tcp_server.py` from the command line.
 if __name__ == "__main__":
-    start_tcp_server()
+    start_threaded_tcp_server()
